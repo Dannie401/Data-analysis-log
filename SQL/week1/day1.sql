8c73f63fe09c00563bc607dbfd86ea07ad0ed7f5
Dannie <dayeon032514@gmail.com>
2026-01-19 22:25:59 +0900

    test: update day1.sql

--- SQL/week1/day1.sql
+++ SQL/week1/day1.sql
@@ -1,18 +1,1 @@
--- Week 1 Day 1
--- Topic: SQL Level 1 - SELECT / WHERE / GROUP BY
--- Source: Programmers Lv.1
-
--- 1) 전체 조회
-SELECT *
-FROM sample_table
-LIMIT 10;
-
--- 2) 조건 조회
-SELECT *
-FROM sample_table
-WHERE condition_column = 'value';
-
--- 3) 집계
-SELECT category, COUNT(*) AS cnt
-FROM sample_table
-GROUP BY category;
+-- test commit
