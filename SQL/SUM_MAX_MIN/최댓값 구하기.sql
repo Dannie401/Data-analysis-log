8c033a377b358838ac9fe2f2860b274cd24c771c
Dannie401 <dayeon032514@gmail.com>
2026-03-10 16:41:14 +0900

    Add SQL query to find maximum DATETIME

---
+++ SQL/SUM_MAX_MIN/최댓값 구하기.sql
@@ -0,0 +1,19 @@
+-- 문제: 최댓값 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59415
+
+SELECT 
+DATETIME AS 시간
+FROM ANIMAL_INS 
+WHERE DATETIME=(SELECT MAX(DATETIME) FROM ANIMAL_INS)
+
+/*
+SELECT MAX(DATETIME) AS 시간
+FROM ANIMAL_INS
+*/
+
+/*
+SELCET DATETIME AS 시간
+FROM ANIMAL_INS
+ORDER BY DATETIME DESC
+LIMIT 1;
+*/
