756f559ca8890b7f4a298b842ca8f33313a5818a
Dannie401 <dayeon032514@gmail.com>
2026-03-11 17:03:16 +0900

    Create 고양이와 개는 몇 마리 있을까.sql

---
+++ SQL/GROUP_BY/고양이와 개는 몇 마리 있을까.sql
@@ -0,0 +1,19 @@
+-- 문제: 고양이와 개는 몇 마리 있을까
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59040
+
+SELECT
+ANIMAL_TYPE,
+count(*) AS "count"
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE ASC;
+
+/*
+SELECT ANIMAL_TYPE, COUNT(*) AS "count"
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY 
+    CASE WHEN ANIMAL_TYPE = 'Cat' THEN 1 
+         WHEN ANIMAL_TYPE = 'Dog' THEN 2 
+         ELSE 3 END;
+*/
