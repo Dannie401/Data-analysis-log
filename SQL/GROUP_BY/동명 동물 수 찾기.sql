7701c6cdc900b4fc540468f6e884a48779e91a36
Dannie401 <dayeon032514@gmail.com>
2026-03-11 17:16:43 +0900

    Add SQL query to find animals with duplicate names

---
+++ SQL/GROUP_BY/동명 동물 수 찾기.sql
@@ -0,0 +1,10 @@
+-- 문제: 동명 동물 수 찾기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59041
+SELECT 
+NAME,
+COUNT(*) AS "COUNT"
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+GROUP BY NAME
+HAVING COUNT(*) > 1
+ORDER BY NAME;
